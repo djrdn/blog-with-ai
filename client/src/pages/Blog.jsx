be5f49680a1681210b2e75055bc2bf0ef3354257
Sergiy Maksimenko <wassabifromhell@ukr.net>
2025-07-04 20:12:42 +0300

    commentSection

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, blog_data } from '../assets/assets'
+import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Moment from 'moment'
 
@@ -9,14 +9,24 @@ function Blog() {
   const {id} = useParams()
 
   const [data, setData] = useState(null)
+  const [comments, setComments] = useState(null)
 
   const fetchBlogData = async ()=> {
     const data = blog_data.find(item => item._id === id)
     setData(data)
   }
 
+  const fetchComments = async ()=> {
+    setComments(comments_data)
+  }
+
+  const addComment = async (e)=> {
+    e.preventDefault();
+  }
+
   useEffect(()=>{
     fetchBlogData()
+    fetchComments()
   }, [])
 
   return data ? (
@@ -38,7 +48,33 @@ function Blog() {
 
         {/* Comments */}
         <div className='mt-14 mb-10 max-w-3xl mx-auto'>
-          <p>Comments</p>
+          <p className='font-semibold mb-4'>Comments ({comments.length})</p>
+          <div className='flex flex-col gap-4'>
+            {comments.map((item, index)=>(
+              <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'>
+                <div className='flex items-center gap-2 mb-2'>
+                  <img src={assets.user_icon} alt="" className='w-6'/>
+                  <p className='forn-medium'>{item.name}</p>
+                </div>
+                <p className='text-sm max-w-md-ml-8'>{item.content}</p>
+                <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Comment Section */}
+        <div className='max-w-3xl mx-auto'>
+          <p className='font-semibold mb-4'>Add your comment</p>
+          <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+            <input type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded outline-none'/>
+
+            <textarea 
+              placeholder='Comment' className='w-full p-2 border border-gray-300 rounded outline-none h-48' required>
+            </textarea>
+
+            <button type='submit' className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'>Submit</button>
+          </form>
         </div>
       </div>
 
