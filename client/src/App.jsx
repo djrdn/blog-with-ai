25f2f2eab8ecca59e3fa49e06c22382fdb232d84
SapFaer <garkavijn@gmail.com>
2025-07-07 16:08:05 +0300

    login and sidebar for admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from "./pages/Blog";
-
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/AddBlog";
+import ListBlog from "./pages/admin/ListBlog";
+import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 const App = () => {
   return(
     <div>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/blog/:id' element={<Blog/>} />
+        <Route path='/admin' element={false ? <Layout/> : <Login/>}>
+        <Route index element={<Dashboard/>}/>
+          <Route path='AddBlog' element={<AddBlog/>} />
+          <Route path='ListBlog' element={<ListBlog/>} />
+          <Route path='comments' element={<Comments/>} />
+        </Route>
       </Routes>
     </div>
   )
