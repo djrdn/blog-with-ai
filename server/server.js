ffc8f04d2de38443d300c552d27035956f301e87
Dmytro <dimandinchik@gmail.com>
2025-07-09 18:11:22 +0300

    start server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+
+const app = express();
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/', (req,res)=> res.send("API is working"))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port ' + PORT)
+})
+
+export default app;
