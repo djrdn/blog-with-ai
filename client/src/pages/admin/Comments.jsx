637e1b1819931394fc2fb61383a98c60d9a7ca49
Dmytro <dimandinchik@gmail.com>
2025-07-17 01:30:47 +0300

    fix commenttableitem import2

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { comments_data } from '../../assets/assets'
-import CommentTableItem from '../../components/admin/СommentTableItem'
+import CommentTableItem from '../../components/admin/CommentTableItem'
 import { useAppContext } from '../../context/AppContext'
 import toast from 'react-hot-toast'
 
